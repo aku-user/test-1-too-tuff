c355e956e8382147229e8cdd2ee070f5cf06a6f1
aku-user <alvino.afzaalghani@gmail.com>
2025-09-26 09:39:34 +0700

    ts so tuff

---
+++ app/quiz/page.tsx
@@ -0,0 +1,11 @@
+export default function Quiz() {
+    return (
+<main className="sejarah-container">
+      
+      <h1 className="judul-sejarah">Quiz Sedulur Sikep</h1>
+
+      </main>
+
+    )
+
+}
