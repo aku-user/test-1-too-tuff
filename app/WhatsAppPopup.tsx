0b259213166f8bb1c49613d7a2849e5ed338d999
aku-user <alvino.afzaalghani@gmail.com>
2025-09-30 18:04:56 +0700

    link

--- app/WhatsAppPopup.tsx
+++ app/WhatsAppPopup.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function WhatsAppPopup() {
   return (
     <a
-      href="https://wa.me/" // ganti nomor WA kamu
+      href="https://chat.whatsapp.com/JQqLhR7Wx8e5cABlcMW8He?mode=ems_copy_t/" // ganti nomor WA kamu
       target="_blank"
       rel="noopener noreferrer"
       className="popup-wa"
