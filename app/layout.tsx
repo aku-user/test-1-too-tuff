ac4afa5c05e37c72372824f16ea175e7d895840b
aku-user <alvino.afzaalghani@gmail.com>
2025-09-30 20:35:27 +0700

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar from "./navbar"; 
-import Footer from "./footer"; 
+import Navbar from "./navbar";
+import Footer from "./footer";
 import WhatsAppPopup from "./WhatsAppPopup";
 
 export const metadata: Metadata = {
@@ -16,12 +16,36 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Sedulur Gen</title>
+       {/* title gambar */}
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="128x128" href="/favicon-128x128.png" />
+        <link rel="icon" type="image/png" sizes="256x256" href="/favicon-256x256.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/favicon-512x512.png" />
+
+        {/* SVG fallback */}
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+
+        {/* Shortcut icon */}
+        <link rel="shortcut icon" href="/favicon.ico" />
+
+        {/* Apple / iOS  */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+
+        {/* Manifest untuk PWA */}
+        <link rel="manifest" href="/site.webmanifest" />
+
+        {/* Windows tile */}
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+      </head>
       <body className="m-0 p-0">
         <Navbar />
         <main className="m-0 p-0">{children}</main>
         <Footer />
-
-        {/* ✅ taruh popup di dalam body */}
         <WhatsAppPopup />
       </body>
     </html>
